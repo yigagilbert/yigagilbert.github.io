a23232309353398560f6ec4eaacb7017969404c9
gilbertyiga <vigidaiz15@gmail.com>
2025-10-01 10:21:42 +0300

    updates

--- mail/contact.php
+++ mail/contact.php
@@ -9,11 +9,12 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "info@example.com"; // Change this email to your //
+$to = "gilbertyiga15@gmail.com"; // Your actual email address
 $subject = "$m_subject:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
-$header = "From: $email";
-$header .= "Reply-To: $email";	
+$header = "From: noreply@yourdomain.com\r\n";
+$header .= "Reply-To: $email\r\n";
+$header .= "Content-Type: text/plain; charset=UTF-8\r\n";	
 
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
